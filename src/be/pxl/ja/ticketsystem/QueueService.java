072a1b315b836e12a80e14a55ecbb00c46814511
Casper Naudts <cn230799@stud.miniemeninstituut.be>
2018-11-26 14:14:58 +0100

    done for the les

--- src/be/pxl/ja/ticketsystem/QueueService.java
+++ src/be/pxl/ja/ticketsystem/QueueService.java
@@ -2,4 +2,27 @@ package be.pxl.ja.ticketsystem;
 
 public class QueueService {
 
+    public void addToQueue(int eventID, User user) {
+
+    }
+
+    public void getQueue(int eventId) {
+
+    }
+
+    public void getNextInLine(int eventID) {
+
+    }
+
+    public void removeFromQueue(int eventID) {
+
+    }
+
+    public void printQueue(int eventID) {
+
+    }
+
+    public void getQueueSize(int eventID) {
+
+    }
 }
---
+++ src/be/pxl/ja/ticketsystem/TicketSystem.java
@@ -0,0 +1,11 @@
+package be.pxl.ja.ticketsystem;
+
+public class TicketSystem {
+    public void requestTicket(Event event, User user) {
+
+    }
+
+    public void viewNext(int eventId) {
+
+    }
+}
