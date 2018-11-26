2e6d533ab6347772eea39f6a8f6e00540a07257a
Casper Naudts <cn230799@stud.miniemeninstituut.be>
2018-11-26 14:03:56 +0100

    add classes

---
+++ src/be/pxl/ja/ticketsystem/Main.java
@@ -0,0 +1,7 @@
+package be.pxl.ja.ticketsystem;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/be/pxl/ja/ticketsystem/QueueService.java
@@ -0,0 +1,5 @@
+package be.pxl.ja.ticketsystem;
+
+public class QueueService {
+
+}
